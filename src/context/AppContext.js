5cad2a54dcb35e573e78b5151b02f288f0a58e00
sitangshu chand <sitangshu.chand@outlook.com>
2023-08-15 01:56:10 -0400

    Added Exercise files

--- src/context/AppContext.js
+++ src/context/AppContext.js
@@ -1,1 +1,89 @@
 import React, { createContext, useReducer } from 'react';
+const AppReducer=(state,action)=>{
+    let new_expenses=[];
+    switch(action.type){
+        case 'ADD_QUANTITY':
+            let updatedqty=false;
+            state.expenses.map((expense=>{
+                if(expense.name=== action.payload.name){
+                    expense.quantity= expense.quanity+ action.payload.quantity;
+                }
+                new_expenses.push(expense);
+                return true;
+            }))
+            state.expenses= new_expenses;
+            action.type="DONE";
+            return {
+                ...state
+            }
+        case "RED_QUANTITY":
+            state.expenses.map((expense)=>{
+                if(expense.name === action.payload.name){
+                    expense.quantity= expense.quantity - action.payload.quantity;
+                }
+                expense.quanity= expense.quanity <0? 0: expense.quanity;
+                new_expenses.push(expense);
+                return true;
+            })
+            state.expenses=new_expenses;
+            action.type="DONE";
+            return {
+                ...state
+            }
+        case "DELETE_ITEM":
+            state.expenses.map((expense)=>{
+                if(expense.name=== action.payload.name){
+                    expense.quantity=0;
+                }
+                new_expenses.push(expense);
+
+                return true;
+            });
+            state.expenses= new_expenses;
+            return {
+                ...state
+            }
+        case "CHG_LOCATION":
+            action.type="DONE";
+            state.Location= action.payload;
+            return {
+                ...state
+            }
+        default :
+            return state;
+    }
+
+}
+
+const initialState={
+    expenses:[{ id: "Shirt", name: 'Shirt', quantity: 0, unitprice: 500 },
+    { id: "Jeans", name: 'Jeans', quantity: 0, unitprice: 300 },
+    { id: "Dress", name: 'Dress', quantity: 0, unitprice: 400 },
+    { id: "Dinner set", name: 'Dinner set', quantity: 0, unitprice: 600 },
+    { id: "Bags", name: 'Bags', quantity: 0, unitprice: 200 },],
+    Location: '£'
+}
+
+export const AppContext=createContext()
+
+export const AppProvider=(props)=>{
+
+    const [state,dispatch]= useReducer(AppReducer, initialState);
+
+    const totalExpenses= state.expenses.reduce((total,item)=>{
+        return (total= total +(item.unitprice*item.quantity))
+    },0)
+    state.CartValue= totalExpenses;
+    return (
+        <AppContext.Provider
+            value={{
+                expenses: state.expenses,
+                CartValue: state.CartValue,
+                dispatch,
+                Location: state.Location
+            }}
+        >
+            {props.children}
+        </AppContext.Provider>
+    )
+}
